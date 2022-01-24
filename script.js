e8312bebf425169bae80c714563d8936de4c7923
JolaWalach <joanna.walach@op.pl>
2022-01-24 18:17:09 +0100

    basic template

---
+++ script.js
@@ -0,0 +1,44 @@
+async function fetchApi() {
+    const API_KEY = "24535628-229b3f069e1ea73ddfc437f03";
+
+    await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=travel&image_type=photo&pretty=true`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            showPhotos(data);
+            createGrid();
+            makeLayout();
+        })
+
+        .catch((err) => {
+            console.log(err)
+        });
+
+    function createGrid() {
+        const container = document.querySelector(".container");
+        container.style.display = "grid";
+        container.style.gridTemplateColumns = "auto auto auto auto auto";
+        container.style.gridTemplateRows = "auto auto auto auto auto";
+        container.style.gridAutoFlow = "row";
+        container.style.gridGap = "30px";
+    }
+
+    function makeLayout() {
+        const container = document.querySelector(".container");
+        const imageDiv = document.createElement("div");
+        imageDiv.className = "imageDiv";
+        //imageDiv.style.gridColumn = "1/span 2";
+        container.appendChild(imageDiv);
+    }
+
+    function showPhotos(data) {
+        const container = document.querySelector(".container");
+        data.hits.forEach(item => {
+            const image = document.createElement("img");
+            image.src = item.webformatURL;
+            //image.style.gridColumn = "1/ 2"
+            container.appendChild(image);
+        })
+    }
+}
+fetchApi();
